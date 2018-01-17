b2c6e5c0f2bab14168df09c4e4a9bedba6825d96
iD3V <iprodev78@gmail.com>
2018-01-17 18:20:10 +0300

    updated pending status of wallet

--- Multy/Model/Realm/UserWalletRLM.swift
+++ Multy/Model/Realm/UserWalletRLM.swift
@@ -112,6 +112,26 @@ class UserWalletRLM: Object {
         return addresses
     }
     
+    func calculateBlockedAmount() -> UInt32 {
+        var sum = UInt32(0)
+        
+        for address in self.addresses {
+            for out in address.spendableOutput {
+                if out.transactionStatus.intValue == TxStatus.MempoolIncoming.rawValue {
+                    sum += out.transactionOutAmount.uint32Value
+                } else if out.transactionStatus.intValue == TxStatus.MempoolOutcoming.rawValue {
+                    let addresses = self.fetchAddresses()
+                    
+                    if addresses.contains(address.address) {
+                        sum += out.transactionOutAmount.uint32Value
+                    }
+                }
+            }
+        }
+        
+        return sum
+    }
+    
     override class func primaryKey() -> String? {
         return "id"
     }
