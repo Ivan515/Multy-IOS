7f36814f99c60efd411e3f66550fc075103f6bbe
Alex Pro <alex78pro@gmail.com>
2018-02-08 17:33:07 +0300

    updated server url

--- Multy/Extensions/Constants.swift
+++ Multy/Extensions/Constants.swift
@@ -77,10 +77,10 @@ enum TxStatus : Int {
 }
 
 //API REST constants
-let apiUrl = "http://88.198.47.112:2278/"//"http://192.168.0.121:7778/"
-//let apiUrl = "https://api.multy.io/"
-//let socketUrl = apiUrl
-let socketUrl = "http://88.198.47.112:2280"
+//let apiUrl = "http://88.198.47.112:2278/"//"http://192.168.0.121:7778/"
+let apiUrl = "https://test.multy.io/"
+let socketUrl = "wss://test.multy.io/"
+//let socketUrl = "http://88.198.47.112:2280"
 let apiUrlTest = "http://192.168.0.125:8080/"
 let nonLocalURL = "http://88.198.47.112:7778/"
 
